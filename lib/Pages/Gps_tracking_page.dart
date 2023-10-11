480381a5e8fbb68e2c0ea91566267a3b3a480f2d
Jexcm <54919774+justincaamano@users.noreply.github.com>
2023-10-11 09:28:37 -0400

    Se actualizo el mapa
    Se agrego nuevos botones bonitos :)

--- lib/Pages/Gps_tracking_page.dart
+++ lib/Pages/Gps_tracking_page.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:permission_handler/permission_handler.dart' as permission;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'gps_sharing.dart';
 
 class TrackingPage extends StatefulWidget {
   const TrackingPage({
@@ -16,34 +19,35 @@ class TrackingPage extends StatefulWidget {
 
 class _TrackingPageState extends State<TrackingPage> {
   final Completer<GoogleMapController> _controller = Completer();
-
-  LatLng? santoDomingoLocation; // Coordenada de Santo Domingo
-  LatLng? santiagoLocation; // Coordenada de Santiago
-
+  LatLng? userLocation;
+  LatLng? otherUserLocation; // Store other user's location
   String locationText = '';
   String distanceText = '';
-  bool showPolyline = false; // Controla si se deben mostrar los polígonos
-  bool isLocationEnabled =
-      false; // Controla si se habilita la obtención de ubicación
+  bool showPolyline = false;
+  bool isLocationEnabled = false;
+  bool showUserMarkers = false;
+  bool personButtonEnabled = false;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  CollectionReference _userLocations =
+      FirebaseFirestore.instance.collection('user_locations');
 
   final AssetsAudioPlayer assetsAudioPlayer = AssetsAudioPlayer();
-
-  // Define the initial coordinates for the static direction line
   List<LatLng> directionCoordinates = [
     LatLng(18.4796, -69.8908),
     LatLng(19.4517000, -70.6970300),
   ];
 
-  // Define the initial coordinates for the moving polyline
   List<LatLng> movingCoordinates = [
-    LatLng(18.4796, -69.8908), // Starting point
+    LatLng(18.4796, -69.8908),
   ];
 
   late StreamSubscription<int> timerSubscription;
-  int updateInterval = 5000; // Update every 5 seconds
-
-  bool isButtonSoundEnabled =
-      true; // Controla si se debe reproducir el sonido al presionar los botones
+  int updateInterval = 5000;
+  bool isButtonSoundEnabled = true;
+  bool locationAccessGranted = false;
+  bool hasReceivedLocation = false;
+  bool hasConsentToShareLocation =
+      false; // Track if the other user consents to share location
 
   @override
   void initState() {
@@ -61,29 +65,43 @@ class _TrackingPageState extends State<TrackingPage> {
     distanceText = 'Destino Final: Santiago';
   }
 
-  void onLocationButtonPressed() {
-    if (isLocationEnabled) {
-      // Define las coordenadas de Santo Domingo y Santiago cuando se presiona el botón
-      santoDomingoLocation = LatLng(18.4796, -69.8908);
-      santiagoLocation = LatLng(19.4517000, -70.6970300);
+  Future<void> _determineInitialLocation() async {
+    Position? position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
 
-      // Muestra los polígonos
-      setState(() {
-        showPolyline = true;
-      });
-    }
+    LatLng initialLatLng = position != null
+        ? LatLng(position.latitude, position.longitude)
+        : LatLng(18.4796, -69.8908);
+
+    setState(() {
+      userLocation = initialLatLng;
+    });
   }
 
-  void onSendLocationButtonPressed() {
-    // Aquí puedes implementar la lógica para enviar ubicación.
-    // Esto podría involucrar el uso de servicios de ubicación en tiempo real o intercambio de coordenadas.
+  Future<void> onLocationButtonPressed() async {
+    final permission.PermissionStatus status =
+        await permission.Permission.location.request();
+
+    if (status == permission.PermissionStatus.granted) {
+      await _determineInitialLocation();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Ubicación actualizada'),
+      ));
+      _saveUserLocation(userLocation!); // Save the location data
+      _showUserRoute(); // Show user's route
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('No tienes acceso a esta función'),
+      ));
+    }
   }
 
   Future<void> playAlertSound() async {
     try {
       await assetsAudioPlayer.open(
         Audio(
-            'assets/onlymp3.to - Danger Alarm Sound Effect-rUkzZTGE6jI-192k-1695664567.mp3'),
+            'assets/onlymp3.to - Danger Alarm Sound Effect-rUkzTGE6jI-192k-1695664567.mp3'),
         autoStart: true,
       );
     } catch (e) {
@@ -91,23 +109,121 @@ class _TrackingPageState extends State<TrackingPage> {
     }
   }
 
-  void onReceiveLocationButtonPressed() async {
-    // Aquí puedes implementar la lógica para recibir ubicación.
-    // Esto podría involucrar el uso de servicios de ubicación en tiempo real o intercambio de coordenadas.
+  Future<void> _saveUserLocation(LatLng location) async {
+    if (location != null) {
+      try {
+        await _userLocations.add({
+          'latitude': location.latitude,
+          'longitude': location.longitude,
+          'timestamp': FieldValue.serverTimestamp(),
+        });
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Ubicación guardada en Firestore'),
+        ));
+      } catch (e) {
+        print('Error saving user location: $e');
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Error al guardar la ubicación'),
+        ));
+      }
+    }
+  }
 
-    // Habilita la obtención de ubicación después de presionar el botón "Recibir Ubicación"
+  Future<void> _showOtherUserLocationDialog() async {
+    final consent = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Solicitar ubicación'),
+          content: Text('¿Deseas compartir tu ubicación con el otro usuario?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false); // No consent
+              },
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true); // Consent
+              },
+              child: Text('Sí'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (consent == true) {
+      // The other user consents to share location
+      setState(() {
+        showUserMarkers = true;
+        hasConsentToShareLocation = true;
+      });
+    }
+  }
+
+  void onReceiveLocationButtonPressed() async {
     setState(() {
       isLocationEnabled = true;
+      locationAccessGranted = true;
+      hasReceivedLocation = true;
     });
   }
 
   void onPanicButtonPressed() {
-    // Play the alert sound when the Panic button is pressed
-    if (isButtonSoundEnabled) {
+    if (!hasReceivedLocation) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('No tienes permiso para esta función'),
+      ));
+    } else if (isButtonSoundEnabled) {
       playAlertSound();
     }
 
-    // Your logic for the Panic button (if any)
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Panic Button Pressed'),
+          content: Text('Your panic action goes here.'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Set<Marker> _createUserMarkers() {
+    final markers = <Marker>{
+      if (userLocation != null)
+        Marker(
+          markerId: const MarkerId("userLocation"),
+          position: userLocation!,
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueBlue,
+          ),
+        ),
+    };
+
+    if (otherUserLocation != null && hasConsentToShareLocation) {
+      markers.add(
+        Marker(
+          markerId: const MarkerId("otherUserLocation"),
+          position: otherUserLocation!,
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueRed,
+          ),
+        ),
+      );
+    }
+
+    return markers;
   }
 
   void startMovingPolyline() {
@@ -115,12 +231,11 @@ class _TrackingPageState extends State<TrackingPage> {
       Duration(milliseconds: updateInterval),
       (iteration) => iteration,
     ).listen((_) {
-      // Update the coordinates of the moving polyline
       if (movingCoordinates.isNotEmpty) {
         final List<LatLng> updatedCoordinates = movingCoordinates.map((coord) {
           return LatLng(
-            coord.latitude + 0.01, // Update latitude
-            coord.longitude + 0.01, // Update longitude
+            coord.latitude + 0.01,
+            coord.longitude + 0.01,
           );
         }).toList();
 
@@ -128,6 +243,12 @@ class _TrackingPageState extends State<TrackingPage> {
           movingCoordinates = updatedCoordinates;
         });
       }
+    });
+  }
+
+  void _showUserRoute() {
+    setState(() {
+      showPolyline = true;
     });
   }
 
@@ -151,14 +272,14 @@ class _TrackingPageState extends State<TrackingPage> {
         children: [
           GoogleMap(
             initialCameraPosition: CameraPosition(
-              target: santoDomingoLocation ?? LatLng(0, 0),
+              target: userLocation ?? LatLng(18.4796, -69.8908),
               zoom: 6.0,
             ),
             polylines: {
               if (showPolyline)
                 Polyline(
                   polylineId: PolylineId("route"),
-                  points: directionCoordinates,
+                  points: [userLocation!, directionCoordinates[0]],
                   color: Colors.blue,
                   width: 6,
                 ),
@@ -169,26 +290,11 @@ class _TrackingPageState extends State<TrackingPage> {
                 width: 6,
               ),
             },
-            markers: {
-              if (santoDomingoLocation != null)
-                Marker(
-                  markerId: const MarkerId("santoDomingo"),
-                  position: santoDomingoLocation!,
-                  icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueGreen,
-                  ),
-                ),
-              if (santiagoLocation != null)
-                Marker(
-                  markerId: const MarkerId("santiago"),
-                  position: santiagoLocation!,
-                ),
-            },
+            markers: _createUserMarkers(),
             onMapCreated: (mapController) {
               _controller.complete(mapController);
             },
           ),
-          // Notification button (Panic button)
           Positioned(
             left: 6.0,
             bottom: 30.0,
@@ -206,21 +312,11 @@ class _TrackingPageState extends State<TrackingPage> {
               ),
             ),
           ),
-          // Live Location button
           Positioned(
             left: 6.0,
             bottom: 85.0,
             child: ElevatedButton(
-              onPressed: () async {
-                // Solicita permiso para acceder a la ubicación en vivo
-                final status = await Permission.location.request();
-
-                if (status == PermissionStatus.granted) {
-                  // El usuario concedió permiso, aquí puedes implementar la lógica para acceder a la ubicación en vivo.
-                } else {
-                  // El usuario no concedió permiso, puedes mostrar un mensaje de error o realizar alguna otra acción.
-                }
-              },
+              onPressed: onLocationButtonPressed,
               child: Icon(
                 Icons.location_on,
                 size: 20.0,
@@ -229,7 +325,42 @@ class _TrackingPageState extends State<TrackingPage> {
                 primary: Colors.blue,
                 onPrimary: Colors.white,
                 shape: CircleBorder(),
-                padding: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(15.0),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 1.0,
+            bottom: 100.0,
+            child: ElevatedButton(
+              onPressed: () async {
+                if (hasReceivedLocation) {
+                  final permission.PermissionStatus status =
+                      await permission.Permission.location.request();
+
+                  if (status == permission.PermissionStatus.granted) {
+                    await _showOtherUserLocationDialog();
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('No tienes permiso para esta función'),
+                    ));
+                  }
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('No tienes permiso para esta función'),
+                  ));
+                }
+              },
+              child: Icon(
+                Icons.person_2,
+                size: 20.0,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orangeAccent,
+                onPrimary: Colors.white,
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15.0),
               ),
             ),
           ),
@@ -242,37 +373,33 @@ class _TrackingPageState extends State<TrackingPage> {
           children: [
             ElevatedButton(
               onPressed: onLocationButtonPressed,
-              child: Text("Obtener Ubicación"),
               style: ElevatedButton.styleFrom(
                 primary: Colors.blue,
                 onPrimary: Colors.white,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 5,
-                  vertical: 5,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5.0,
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: onSendLocationButtonPressed,
-              child: Text("Enviar Ubicación"),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.green,
-                onPrimary: Colors.white,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 0.5,
-                  vertical: 0.5,
-                ),
+              child: Text(
+                locationAccessGranted
+                    ? 'Ubicación actualizada'
+                    : 'Obtener Ubicación',
               ),
             ),
             ElevatedButton(
               onPressed: onReceiveLocationButtonPressed,
-              child: Text("Recibir Ubicación"),
+              child: Text(
+                hasReceivedLocation
+                    ? 'Haz recibido la ubicación'
+                    : 'Recibir Ubicación',
+              ),
               style: ElevatedButton.styleFrom(
                 primary: Colors.orange,
                 onPrimary: Colors.white,
-                padding: EdgeInsets.symmetric(
-                  horizontal: 5,
-                  vertical: 5,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5.0,
                 ),
               ),
             ),
